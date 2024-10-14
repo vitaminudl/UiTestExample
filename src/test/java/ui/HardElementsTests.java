042a2d403e084c732a60706d06355a0671b8c5d8
VitaliyD <vitaminudl@gmail.com>
2024-10-14 16:48:57 +0300

    test:(<номер тикета jira>): Добавлена начальная версия UI тестов

---
+++ src/test/java/ui/HardElementsTests.java
@@ -0,0 +1,124 @@
+package ui;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.*;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Action;
+import org.openqa.selenium.interactions.Actions;
+
+import java.time.Duration;
+import java.util.List;
+
+
+public class HardElementsTests {
+    private WebDriver driver;
+
+    @BeforeAll
+    public static void downloadDriver(){
+        WebDriverManager.chromedriver().setup();
+    }
+
+    @BeforeEach
+    public void setUp(){
+        driver = new ChromeDriver();
+        driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(20));
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
+    }
+
+    @AfterEach
+    public void tearDown(){
+        driver.close();
+    }
+
+    @Test
+    public void authTest(){
+        driver.get("https://admin:admin@the-internet.herokuapp.com/basic_auth");
+        String h3= driver.findElement(By.xpath("//h3")).getText();
+        Assertions.assertEquals("Basic Auth", h3);
+    }
+
+    @Test
+    public void alertTest(){
+        String expectedTest = "I am a JS Alert";
+        String expectedResult = "You successfully clicked an alert";
+
+        driver.get("https://the-internet.herokuapp.com/javascript_alerts");
+        driver.findElement(By.xpath("//button[@onclick='jsAlert()']")).click();
+        String actualText = driver.switchTo().alert().getText();
+        driver.switchTo().alert().accept();
+        String result = driver.findElement(By.id("result")).getText();
+        Assertions.assertEquals(expectedTest, actualText);
+        Assertions.assertEquals(expectedResult, result);
+    }
+
+    @Test
+    public void iframeTest(){
+        driver.get("https://mail.ru/");
+        driver.findElement(By.xpath("//button[@data-click-counter='75068996']")).click();
+        WebElement iframeAuth = driver.findElement(By.xpath("//iframe[@class='ag-popup__frame__layout__iframe']"));
+        driver.switchTo().frame(iframeAuth);
+        driver.findElement(By.xpath("//input[@name='username']")).sendKeys("test@mail.ru");
+    }
+
+    @Test
+    public void selectMrTest(){
+        driver.get("http://85.192.34.140:8081/");
+        WebElement  elementsCard = driver.findElement(By.xpath("//div[@class='card-body']//h5[text()='Widgets']"));
+        elementsCard.click();
+        WebElement elementsTextBox = driver.findElement(By.xpath("//span[text()='Select Menu']"));
+        elementsTextBox.click();
+
+        driver.findElement(By.xpath("//div[@class=' css-1hwfws3']//following::div[text()='Select Title']")).click();
+        driver.findElement(By.xpath("//div[text()='Pick one title']//following::div[text()='Mr.']")).click();
+    }
+
+
+    @Test
+    public void sliderTest(){
+        driver.get("http://85.192.34.140:8081/");
+        WebElement  elementsCard = driver.findElement(By.xpath("//div[@class='card-body']//h5[text()='Widgets']"));
+        elementsCard.click();
+        WebElement elementsTextBox = driver.findElement(By.xpath("//span[text()='Slider']"));
+        elementsTextBox.click();
+
+        WebElement slider = driver.findElement(By.xpath("//input[@type='range']"));
+//        Actions actions = new Actions(driver);
+//        actions.dragAndDropBy(slider, 50,0).build().perform();
+
+        int expectedValue = 85;
+        int currentValue = Integer.parseInt(slider.getAttribute("value"));
+        int valueToMove = expectedValue - currentValue;
+        for (int i = 0; i < valueToMove ; i++) {
+            slider.sendKeys(Keys.ARROW_RIGHT);
+        }
+
+        WebElement sliderValue = driver.findElement(By.id("sliderValue"));
+        int actualValue = Integer.parseInt(sliderValue.getAttribute("value"));
+        Assertions.assertEquals(expectedValue, actualValue);
+
+    }
+
+    @Test
+    public void hoverTest(){
+        driver.get("http://85.192.34.140:8081/");
+        WebElement  elementsCard = driver.findElement(By.xpath("//div[@class='card-body']//h5[text()='Widgets']"));
+        elementsCard.click();
+        WebElement elementsTextBox = driver.findElement(By.xpath("//span[text()='Menu']"));
+        elementsTextBox.click();
+
+        WebElement menuItemMiddle = driver.findElement(By.xpath("//a[text()='Main Item 2']"));
+        Actions actions = new Actions(driver);
+        actions.moveToElement(menuItemMiddle).build().perform();
+
+        WebElement subSubList = driver.findElement(By.xpath("//a[text()='SUB SUB LIST »']"));
+        actions.moveToElement(subSubList).build().perform();
+
+        List<WebElement> lastElements = driver.findElements(By.xpath("//a[contains(text(), 'Sub Sub Item')]"));
+         Assertions.assertEquals(2,lastElements.size());
+    }
+
+}
---
+++ src/test/java/ui/SeleniumTests.java
@@ -0,0 +1,130 @@
+package ui;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.*;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Dimension;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.io.File;
+import java.nio.file.Paths;
+import java.time.Duration;
+import java.util.HashMap;
+import java.util.Map;
+
+public class SeleniumTests {
+    private WebDriver driver;
+    private String downloadFolder = System.getProperty("user.dir") + File.separator + "build" + File.separator + "downloadFiles";
+
+    @BeforeAll
+    public static void downloadDriver(){
+        WebDriverManager.chromedriver().setup();
+    }
+
+    @BeforeEach
+    public void setUp(){
+        ChromeOptions options = new ChromeOptions();
+        Map<String,String> prefs = new HashMap<>();
+        prefs.put("download.default_directory", downloadFolder);
+        options.setExperimentalOption("prefs", prefs);
+
+        driver = new ChromeDriver(options);
+        driver.manage().window().setSize(new Dimension(1920, 1080));
+        driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(20));
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
+    }
+
+    @AfterEach
+    public void tearDown(){
+        driver.close();
+    }
+
+    @Test
+    public void simpleUiTest(){
+        String expectedTitle = "Олег Пендрак - Инженер по автоматизации тестирования QA Automation";
+
+        driver.get("https://threadqa.ru");
+        String actualTittle = driver.getTitle();
+
+        Assertions.assertEquals(expectedTitle,actualTittle);
+    }
+
+    @Test
+    public void simpleFromTest(){
+        String expectedName = "Tomas Anderson";
+        String expectedEmail = "tomas@matrix.ru";
+        String expectedCurrentAndress = "USA Los Angeles";
+        String expectedPermanentAddress = "USA Miami";
+
+        driver.get("http://85.192.34.140:8081/");
+        WebElement  elementsCard = driver.findElement(By.xpath("//div[@class='card-body']//h5[text()='Elements']"));
+        elementsCard.click();
+        WebElement elementsTextBox = driver.findElement(By.xpath("//span[@class='text']"));
+        elementsTextBox.click();
+
+        WebElement fullName = driver.findElement(By.id("userName"));
+        WebElement email = driver.findElement(By.id("userEmail"));
+        WebElement currentAddress = driver.findElement(By.id("currentAddress"));
+        WebElement permanentAddress = driver.findElement(By.id("permanentAddress"));
+        WebElement submit = driver.findElement(By.id("submit"));
+
+        fullName.sendKeys(expectedName);
+        email.sendKeys(expectedEmail);
+        currentAddress.sendKeys(expectedCurrentAndress);
+        permanentAddress.sendKeys(expectedPermanentAddress);
+        submit.click();
+
+        WebElement nameNew = driver.findElement(By.id("name"));
+        WebElement emailNew = driver.findElement(By.id("email"));
+        WebElement currentAddressNew = driver.findElement(By.xpath("//div[@id ='output']//p[@id='currentAddress']"));
+        WebElement permanentAddressNew = driver.findElement(By.xpath("//div[@id ='output']//p[@id='permanentAddress']"));
+
+        String actualName = nameNew.getText();
+        String actualEmail = emailNew.getText();
+        String actualCurrentAddressNew = currentAddressNew.getText();
+        String actualPermanentAddressNew = permanentAddressNew.getText();
+
+        Assertions.assertTrue(actualName.contains(expectedName));
+        Assertions.assertTrue(actualEmail.contains(expectedEmail));
+        Assertions.assertTrue(actualCurrentAddressNew.contains(expectedCurrentAndress));
+        Assertions.assertTrue(actualPermanentAddressNew.contains(expectedPermanentAddress));
+    }
+
+    @Test
+    public void uploadTest(){
+        driver.get("http://85.192.34.140:8081/");
+        WebElement  elementsCard = driver.findElement(By.xpath("//div[@class='card-body']//h5[text()='Elements']"));
+        elementsCard.click();
+        WebElement elementsTextBox = driver.findElement(By.xpath("//span[text()='Upload and Download']"));
+        elementsTextBox.click();
+
+        WebElement uploadBtn = driver.findElement(By.id("uploadFile"));
+        uploadBtn.sendKeys(System.getProperty("user.dir") + "/src/test/resources/UITestsExample – SeleniumTests.java [UITestsExample.test] 2024-10-14 08-13-19.png");
+
+        WebElement uploadedFakePath = driver.findElement(By.id("uploadedFilePath"));
+        Assertions.assertTrue(uploadedFakePath.getText().contains("SeleniumTests.java [UITestsExample.test] 2024-10-14 08-13-19.png"));
+    }
+
+    @Test
+    public void downloadTest(){
+        driver.get("http://85.192.34.140:8081/");
+        WebElement  elementsCard = driver.findElement(By.xpath("//div[@class='card-body']//h5[text()='Elements']"));
+        elementsCard.click();
+        WebElement elementsTextBox = driver.findElement(By.xpath("//span[text()='Upload and Download']"));
+        elementsTextBox.click();
+
+        WebElement dowloadBtn = driver.findElement(By.id("downloadButton"));
+        dowloadBtn.click();
+
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(30));
+        wait.until(x-> Paths.get(downloadFolder,"sticker.png").toFile().exists());
+
+        File file = new File("build/downloadFiles/sticker.png");
+        Assertions.assertTrue(file.length() !=0);
+        Assertions.assertNotNull(file);
+    }
+}
