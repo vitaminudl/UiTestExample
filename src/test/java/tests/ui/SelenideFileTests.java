fbade7a0dc658ea7708d72b8309083d301bdb503
VitaliyD <vitaminudl@gmail.com>
2024-10-16 18:07:35 +0300

    test:(<номер тикета jira>): Добавлены тесты pdf

---
+++ src/test/java/tests/ui/SelenideFileTests.java
@@ -0,0 +1,29 @@
+package tests.ui;
+
+import com.codeborne.pdftest.PDF;
+import com.codeborne.selenide.Selenide;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.io.File;
+import java.io.IOException;
+
+import static com.codeborne.selenide.Selenide.$x;
+
+public class SelenideFileTests {
+    @Test
+    public void readPdfTest() throws IOException {
+        File pdf = new File("src/test/resources/terms-and-conditions.pdf");
+        PDF pdfReader = new PDF(pdf);
+        String pdfText = pdfReader.text;
+        Assertions.assertTrue(pdfText.contains("The regulations of Section 44a of"));
+    }
+
+    @Test
+    public void readPdfFile() throws IOException {
+        Selenide.open("https://www.pdf995.com/samples/");
+        File pdf = $x("//td[@data-sort='pdf.pdf']/a").download();
+        PDF pdfReader = new PDF(pdf);
+        Assertions.assertEquals("Software 995", pdfReader.author);
+    }
+}
